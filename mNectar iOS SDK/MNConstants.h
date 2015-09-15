93ab39f7bfb2faef6f4ab2f8daa5e921ae436cf7
Tony Huynh <tony@mnectar.com>
2015-09-15 14:08:17 -0700

    use https for ad requests, update changelog, update version

--- mNectar iOS SDK/MNConstants.h
+++ mNectar iOS SDK/MNConstants.h
@@ -1,7 +1,11 @@
+#ifndef MN_USE_HTTPS
+#define MN_USE_HTTPS YES
+#endif
+
 #ifndef MN_ENDPOINT
-#define MN_ENDPOINT "http://ads.mnectar.com/m/v1/ad"
+#define MN_ENDPOINT "ads.mnectar.com/m/v1/ad"
 #endif
 
 #ifndef MN_VERSION
-#define MN_VERSION "1.1"
+#define MN_VERSION "1.2"
 #endif
